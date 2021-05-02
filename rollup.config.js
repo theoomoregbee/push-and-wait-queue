a928ea9b57eda5157a1938338db71eff23fdf4e9
Theophilus Omoregbee <theo4u@ymail.com>
2021-05-02 10:06:08 -0400

    chore: update library name and links

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
 import typescript from 'rollup-plugin-typescript2'
-import { name } from './package.json'
 
 export default {
   input: 'lib/index.ts',
   output: {
     file: 'build/index.js',
     format: 'umd',
-    name
+    name: 'PushAndWaitQueue'
   },
   plugins: [typescript({
     tsconfigOverride: {
