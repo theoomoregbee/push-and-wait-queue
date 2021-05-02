41b26a49d61483f698037b9d9518062787754398
Theophilus Omoregbee <theo4u@ymail.com>
2021-05-02 08:14:12 -0400

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import typescript from 'rollup-plugin-typescript2'
+import { name } from './package.json'
+
+export default {
+  input: 'lib/index.ts',
+  output: {
+    file: 'build/index.js',
+    format: 'umd',
+    name
+  },
+  plugins: [typescript({
+    tsconfigOverride: {
+      compilerOptions: { rootDir: './lib' },
+      include: ['lib/**/*'],
+      exclude: ['tests/**/*']
+    }
+  })]
+}
